c46cfc1ebb499611d65349dd32c40211945b52f7
Hawa <hawamaria33@gmail.com>
2023-06-16 16:00:34 +0300

    challenge 2 completed

--- solution1.js
+++ solution1.js
@@ -1,23 +1,16 @@
-let studentGrade = prompt("Enter your grade:")
+let studentGrade = prompt("Enter your grade:");
 
-if (studentGrade >100 || studentGrade< 0){
-    alert ('Invalid Mark')
-} 
-if(studentGrade > 79){
-    alert('You scored A ')
+if (studentGrade > 100 || studentGrade < 0) {
+  alert("Invalid Mark");
 }
-else if(studentGrade >= 60 && studentGrade<=79){
-    alert('You Scored B')
+if (studentGrade > 79) {
+  alert("You scored A ");
+} else if (studentGrade >= 60 && studentGrade <= 79) {
+  alert("You Scored B");
+} else if (studentGrade >= 49 && studentGrade <= 59) {
+  alert("You Scored C");
+} else if (studentGrade >= 40 && studentGrade < 49) {
+  alert("You Scored D");
+} else if (studentGrade < 40) {
+  alert("You Scored E");
 }
-else if(studentGrade >= 49 && studentGrade <= 59){
-    alert('You Scored C')
-}
-else if (studentGrade>= 40 && studentGrade <49 ){
-   alert('You Scored D')
-}
-else {
-    alert('You Scored E')
-}
-
-
-
---
+++ solution2.js
@@ -0,0 +1,14 @@
+let carSpeed = prompt("Car Speed:");
+let speedLimit = 70;
+let kmPerS = 5;
+
+if (carSpeed <= speedLimit) {
+  alert("OK!!");
+} else {
+  const demeritPoints = (carSpeed - speedLimit) / kmPerS;
+  if (demeritPoints > 12) {
+    alert("License suspended");
+  } else {
+    alert("Points" + demeritPoints);
+  }
+}
