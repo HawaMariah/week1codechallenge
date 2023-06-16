f994435ba9202ec720b4a1a5323a4324ae3109d0
Hawa <hawamaria33@gmail.com>
2023-06-16 12:21:43 +0300

    challenge 1 completed

---
+++ solution1.js
@@ -0,0 +1,23 @@
+let studentGrade = prompt("Enter your grade:")
+
+if (studentGrade >100 || studentGrade< 0){
+    alert ('Invalid Mark')
+} 
+if(studentGrade > 79){
+    alert('You scored A ')
+}
+else if(studentGrade >= 60 && studentGrade<=79){
+    alert('You Scored B')
+}
+else if(studentGrade >= 49 && studentGrade <= 59){
+    alert('You Scored C')
+}
+else if (studentGrade>= 40 && studentGrade <49 ){
+   alert('You Scored D')
+}
+else {
+    alert('You Scored E')
+}
+
+
+
