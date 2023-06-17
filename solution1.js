1499e449eeb8624347a6c84b36c7467e8aaf3848
Hawa <hawamaria33@gmail.com>
2023-06-17 10:54:44 +0300

    completed challenge

--- solution1.js
+++ solution1.js
@@ -1,16 +1,18 @@
 let studentGrade = prompt("Enter your marks:");
 
 if (studentGrade > 100 || studentGrade < 0) {
-  alert("Invalid Marks");
+  console.log("Invalid Marks");
 }
-if (studentGrade > 79) {
-  alert("You scored A ");
-} else if (studentGrade >= 60 && studentGrade <= 79) {
-  alert("You Scored B");
+if (studentGrade >= 79 && studentGrade <= 100) {
+  console.log("You scored A ");
+} else if (studentGrade >= 60 && studentGrade < 79) {
+  console.log("You Scored B");
 } else if (studentGrade >= 49 && studentGrade <= 59) {
-  alert("You Scored C");
+  console.log("You Scored C");
 } else if (studentGrade >= 40 && studentGrade < 49) {
-  alert("You Scored D");
-} else if (studentGrade < 40) {
-  alert("You Scored E");
+  console.log("You Scored D");
+} else if (studentGrade >= 0 && studentGrade < 40) {
+  console.log("You Scored E");
+} else {
+  console.log("ERROR!");
 }
